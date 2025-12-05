77e95a4152ca0dbf45cf1c5e8bb4a409b91ade60
mohamedkonneh <konnehmohamed354@gmail.com>
2025-12-05 03:29:35 +0300

    fix(backend): correct dotenv path for db migration script

--- backend/models/database.js
+++ backend/models/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../src/.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const isProduction = process.env.NODE_ENV === 'production';
 
