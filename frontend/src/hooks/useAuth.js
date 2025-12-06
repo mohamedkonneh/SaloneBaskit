9becc1a6462171f0a4fd8494b9ea2a214da2a74b
mohamedkonneh <konnehmohamed354@gmail.com>
2025-12-06 19:20:43 +0300

    Your update message

--- frontend/src/hooks/useAuth.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
-
-// Custom hook to easily access auth context data in any component
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
