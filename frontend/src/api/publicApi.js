a859a986ffac3a2eaccddfc6757e720f1c3033fe
mohamedkonneh <mamabintucia@gmail.com>
2025-12-15 02:00:06 +0300

    Update publicApi.js

--- frontend/src/api/publicApi.js
+++ frontend/src/api/publicApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'https://salonebaskit-993d.onrender.com/api';
+const BACKEND_URL = 'https://salonebaskit-993d.onrender.com';
 
 const publicApi = axios.create({
   baseURL: `${BACKEND_URL}/api`,
